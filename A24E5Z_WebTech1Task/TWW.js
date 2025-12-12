1a3efcc93262994e7da8057e915ccf63b5ad9c0b
TPeter12 <tothpeter0410@gmail.com>
2025-12-12 18:36:49 +0100

    plusz_JS

---
+++ A24E5Z_WebTech1Task/TWW.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const shootSound = new Audio("revolverSOUND.mp3");
+  shootSound.volume = 0.1;
+
+  document.addEventListener("click", e => {
+    const texts = ["BANG!", "POW!"];
+    const bang = document.createElement("div");
+    bang.className = "western-bang";
+    bang.textContent = texts[Math.floor(Math.random() * texts.length)];
+    bang.style.left = e.pageX + "px";
+    bang.style.top  = e.pageY + "px";
+    document.body.appendChild(bang);
+
+    // Hang lejátszása
+    shootSound.currentTime = 0;
+    shootSound.play().catch(() => {});
+
+    setTimeout(() => bang.remove(), 700);
+  });
+});
